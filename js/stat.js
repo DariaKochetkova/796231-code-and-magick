ec282eea65677621e2a294eb9e143d64ef6af1c1
Daria <42115994+DariaKochetkova@users.noreply.github.com>
2018-11-23 03:55:33 +0500

    ваше сообщениеваше сообщение

--- js/stat.js
+++ js/stat.js
@@ -13,6 +13,7 @@ var TEXT_GAP_TOP = 30;
 var NAME_GAP_TOP = 270;
 var SHADOW_GAP = 10;
 var BAR_GAP_TOP = 100;
+var RED_BAR = 'rgba(255, 0, 0, 1)';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -30,26 +31,27 @@ var getMaxElement = function (arr) {
 };
 
 window.renderStatistics = function (ctx, players, times) {
-  var X_TEXT = CLOUD_X + TEXT_GAP_LEFT;
+  var textX = CLOUD_X + TEXT_GAP_LEFT;
   renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
   ctx.fillStyle = '#000';
   ctx.font = '16px, PT Mono';
-  ctx.fillText('Ура вы победили!', X_TEXT, CLOUD_Y + TEXT_GAP_TOP);
-  ctx.fillText('Список результатов:', X_TEXT, CLOUD_Y + TEXT_GAP_TOP * 1.7);
+  ctx.fillText('Ура вы победили!', textX, CLOUD_Y + TEXT_GAP_TOP);
+  ctx.fillText('Список результатов:', textX, CLOUD_Y + TEXT_GAP_TOP * 1.7);
 
   var maxTime = getMaxElement(times);
-  var redBar = 'rgba(255, 0, 0, 1)';
+
   var getRandomBlue = function () {
     return 'rgba(0, 0, 102, ' + Math.random() + ')';
   };
   for (var i = 0; i < players.length; i++) {
-    var X = CLOUD_X + GAP_LEFT + (BAR_WIDTH + BAR_SPACE) * i;
+    var barX = CLOUD_X + GAP_LEFT + (BAR_WIDTH + BAR_SPACE) * i;
     var barHeight = (BAR_HEIGHT * times[i]) / maxTime;
+    var barY = BAR_GAP_TOP + BAR_HEIGHT - barHeight;
     ctx.fillStyle = '#000';
-    ctx.fillText(Math.round(times[i]), X, BAR_GAP_TOP + BAR_HEIGHT - barHeight - 10);
-    ctx.fillText(players[i], X, NAME_GAP_TOP);
-    ctx.fillStyle = players[i] === 'Вы' ? redBar : getRandomBlue();
-    ctx.fillRect(X, BAR_GAP_TOP + BAR_HEIGHT - barHeight, BAR_WIDTH, barHeight);
+    ctx.fillText(Math.round(times[i]), barX, BAR_GAP_TOP + BAR_HEIGHT - barHeight - 10);
+    ctx.fillText(players[i], barX, NAME_GAP_TOP);
+    ctx.fillStyle = players[i] === 'Вы' ? RED_BAR : getRandomBlue();
+    ctx.fillRect(barX, barY, BAR_WIDTH, barHeight);
   }
 };
