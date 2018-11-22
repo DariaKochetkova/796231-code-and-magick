06f2422e1b119f50f3fd37b81564cf5f448de385
Daria <42115994+DariaKochetkova@users.noreply.github.com>
2018-11-23 03:44:01 +0500

    отредактировала второе задание

--- js/stat.js
+++ js/stat.js
@@ -30,24 +30,26 @@ var getMaxElement = function (arr) {
 };
 
 window.renderStatistics = function (ctx, players, times) {
+  var X_TEXT = CLOUD_X + TEXT_GAP_LEFT;
   renderCloud(ctx, CLOUD_X + SHADOW_GAP, CLOUD_Y + SHADOW_GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
   ctx.fillStyle = '#000';
   ctx.font = '16px, PT Mono';
-  ctx.fillText('Ура вы победили!', CLOUD_X + TEXT_GAP_LEFT, CLOUD_Y + TEXT_GAP_TOP);
-  ctx.fillText('Список результатов:', CLOUD_X + TEXT_GAP_LEFT, CLOUD_Y + TEXT_GAP_TOP * 1.7);
+  ctx.fillText('Ура вы победили!', X_TEXT, CLOUD_Y + TEXT_GAP_TOP);
+  ctx.fillText('Список результатов:', X_TEXT, CLOUD_Y + TEXT_GAP_TOP * 1.7);
 
   var maxTime = getMaxElement(times);
-
+  var redBar = 'rgba(255, 0, 0, 1)';
+  var getRandomBlue = function () {
+    return 'rgba(0, 0, 102, ' + Math.random() + ')';
+  };
   for (var i = 0; i < players.length; i++) {
+    var X = CLOUD_X + GAP_LEFT + (BAR_WIDTH + BAR_SPACE) * i;
+    var barHeight = (BAR_HEIGHT * times[i]) / maxTime;
     ctx.fillStyle = '#000';
-    ctx.fillText(Math.round(times[i]), CLOUD_X + GAP_LEFT + (BAR_WIDTH + BAR_SPACE) * i, BAR_GAP_TOP + BAR_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime - 10);
-    ctx.fillText(players[i], CLOUD_X + GAP_LEFT + (BAR_WIDTH + BAR_SPACE) * i, NAME_GAP_TOP);
-    if (players[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'rgba(0, 0, 102, ' + Math.random() + ')';
-    }
-    ctx.fillRect(CLOUD_X + GAP_LEFT + (BAR_WIDTH + BAR_SPACE) * i, BAR_GAP_TOP + BAR_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillText(Math.round(times[i]), X, BAR_GAP_TOP + BAR_HEIGHT - barHeight - 10);
+    ctx.fillText(players[i], X, NAME_GAP_TOP);
+    ctx.fillStyle = players[i] === 'Вы' ? redBar : getRandomBlue();
+    ctx.fillRect(X, BAR_GAP_TOP + BAR_HEIGHT - barHeight, BAR_WIDTH, barHeight);
   }
 };
